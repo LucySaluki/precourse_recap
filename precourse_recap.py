c15330ee30f15406f10f56396160d13c130f182c
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-07-13 19:46:47 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+new_colour=input("What is your favourite colour? ")
+
+if new_colour=="green":
+    result="My favourite colour is green too!"
+elif new_colour=="blue":
+    result="Blue is my second favourite colour (my favourite is green)!"
+else:
+    result="Nah, not a fan of that colour at all."
+
+print(result)
